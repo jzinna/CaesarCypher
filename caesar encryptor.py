5cbc195596cbbe8276202deaf00995c0eec5e830
jzinna <jzinna@gmail.com>
2017-06-04 22:50:14 -0700

    Create caesar encryptor.py

---
+++ caesar encryptor.py
@@ -0,0 +1,4 @@
+''' This program will take a text and a key (a number), and return the text shifted (number) of letters to the right '''
+
+take a file path and name, and load it all into a file object, using readlines().
+take the key and shift the alphabet (number) of letters to the right.
