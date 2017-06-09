54618692f256e340cfe90b92b854c426658ccd37
jzinna <jzinna@gmail.com>
2017-06-09 13:41:52 -0700

    Create caesar encryptor.py

--- caesar encryptor.py
+++ caesar encryptor.py
@@ -1,7 +1,7 @@
 ''' This program will take a text and a key (a number), and return the text shifted (number) of letters to the right '''
 
 import string
-CLASS (list of lines in the text, key)
+class working_text(list of lines in the text, key)
 # class for text to be encrypted.  Will require a string and also the key
     # define two private constants, string.ascii_lowercase and uppercase (these have the alphabet)
     
@@ -14,33 +14,33 @@ CLASS (list of lines in the text, key)
             # ucase shifted alph = ucase alph[(key - 1) to 25] + ucase alph[0 to (key - 2)]
     
     # def encrypt()
-        # iterate through each position, which will be each a line of the text
+        # iterate through each position, which will each be a line of the text
             # on each line, a list can be created, list(line), it should have each character in a different position (check if the... 
                                                         # ...list from the previous iteration is replaced, if not need to wipe it)
             # iterate through each character
-                # check if it's alphanumeric (can list command create blank spaces or newline chars??)        
-                # check if lower or upper case
-                # if lower 
-                    # find its position pos in the unshifted lcase alphabet
-                    # find the new character in the shifted lcase alphabet, using pos
-                # else
-                    # find its position pos in the unshifted ucase alphabet
-                    # find the new character in the shifted ucase alphabet, using pos
+                # get_shifted_char(char)
                 # replace this character in the list (use comprehension??)
                 # replace the original version of the word with the shifted version
         # once we have the whole list of words in the file shifted, convert the list to a string
         # return the string as the response, shifted text
+        
+    # def get_shifted_char(char)
+        # check if it's alphanumeric (can list command create blank spaces or newline chars??)        
+        # check if lower or upper case
+        # if lower 
+            # find its position pos in the unshifted lcase alphabet
+            # find the new character in the shifted lcase alphabet, using pos
+        # else
+            # find its position pos in the unshifted ucase alphabet
+            # find the new character in the shifted ucase alphabet, using pos
 
+main encrypt_Caesar(file_path, key)
+    # file-objet = open given file, from file path
+    # list of strings = file-object.readlines() => each position in the list will be a string with one line of text
+    # create an encrypted file object of class 'working_text', providing the list of strings and the key
+    # call method encrypt, and assign the result (a string) to a variable, and print it, or create a new file with the encrypted message
 
-MAIN (file path, key)
-
-# file-objet = open given file, from file path
-# list of strings = file-object.readlines() => each position in the list will be a string with one line of text
-# create an encrypted file object of class 'encrypted' or however I'll call the class
-# call method encrypt providing the list of strings and the key, and assign the result (a string) to a variable, and print it, or... 
-                                                                        # ...create a new file with the encrypted message
-
-# just for good habit, will close the file-object after coming back from the method and doing stuff with the result
+    # just for good habit, will close the file-object after coming back from the method and doing stuff with the result
 
 
 IDEAS
